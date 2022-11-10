7a3d1c5a0ef2a8fa2b7d387345c63543bf75d44e
Stephen Agius <s.agius@united-remote.com>
2022-11-10 22:13:34 +0100

    Added the header component to move from one route to another

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculatorModule } from './components/calculator/calculator.module';
 import { SuprCalculatorModule } from './components/suprcalculator/suprcalculator.module';
+import { HeaderModule } from './components/header/header.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SuprCalculatorModule } from './components/suprcalculator/suprcalculator
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HeaderModule,
     CalculatorModule,
     SuprCalculatorModule
   ],
---
+++ src/app/components/header/header.component.spec.ts
@@ -0,0 +1,22 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        HeaderComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the header', () => {
+    const fixture = TestBed.createComponent(HeaderComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
